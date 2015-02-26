a106d4ad33aeed3a7aa711bb27ea551daeda51af
Viraj Sinha <virajosinha@gmail.com>
2015-02-25 19:23:48 -0500

    made more scala-idiomatic and added comments

--- src/DiffieHellman.scala
+++ src/DiffieHellman.scala
@@ -5,66 +5,55 @@
 import scala.util.Random
 
 class DiffieHellman (val p: BigInt, val g: Int) {
-  private var privateKey: BigInt = null
-  var peerPublicKey: BigInt = null
+
+  // private key is generated once and for always
+  // (re-generating the private key breaks the algorithm)
+  private val privateKey: BigInt = this.random(1024)
+
   var peerPublicKeys: Array[BigInt] = Array[BigInt]()
 
-  def generatePrivateKey(): Unit = {
-    privateKey = this.random(1024)
-  }
-
   def publicKey: BigInt = {
-    // do this here in case it wasn't done
-    // explicitly earlier
-    if (privateKey == null) {
-      generatePrivateKey()
-    }
-
     // public key is g^privateKey mod p
     expMod(g, privateKey, p)
-  }
-
-  def addPeerPublicKey(key: BigInt) = {
-    // append to the list of keys we have
-    peerPublicKeys :+= key
   }
 
   def sharedSecret: BigInt = {
     // multiply all the keys in the group of participants,
     // starting with our own
-    var secret: BigInt = this.publicKey
-    for (publicKey <- peerPublicKeys) {
-      secret *= publicKey
-    }
-    secret
+    this.publicKey * peerPublicKeys.foldLeft(1: BigInt)((a, b) => a * b)
   }
 
-  def random(n: Int): BigInt = {
-    val rnd = new Random()
-    BigInt(n, rnd)
+  private def addPeerPublicKey(key: BigInt) = {
+    // append the new key to the list of keys we have
+    // this operation should really only be undertaken by the
+    // DiffieHellman.doKeyExchange function.
+    peerPublicKeys :+= key
+  }
+
+  private def random(n: Int): BigInt = {
+    BigInt(n, new Random())
   }
 
   // calculates a^b mod c
-  def expMod(a: BigInt, b: BigInt, c: BigInt): BigInt = {
+  private def expMod(a: BigInt, b: BigInt, c: BigInt): BigInt = {
     a.modPow(b, c)
   }
 }
 
 object DiffieHellman {
   def randomPrime(n: Int): BigInt = {
-    val rand = new Random()
-    BigInt.probablePrime(n, rand)
+    BigInt.probablePrime(n, new Random())
   }
 
   // share all public keys with all participants,
   // for any number of participants
   def doKeyExchange(participants: Array[DiffieHellman]): Unit = {
-    for (i <- 0 until participants.length) {
-      for (j <- 0 until participants.length) {
-        if (i != j) {
-          participants(i).addPeerPublicKey(participants(j).publicKey)
-        }
-      }
+    for {
+      keySender: DiffieHellman <- participants
+      keyReciever: DiffieHellman <- participants
+      if keySender != keyReciever
+    } yield {
+      keyReciever.addPeerPublicKey(keySender.publicKey)
     }
   }
 }
