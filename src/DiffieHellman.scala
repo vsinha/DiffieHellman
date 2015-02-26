95c10eccf87cdffc4259fd65e874df534153775c
Viraj Sinha <virajosinha@gmail.com>
2015-02-25 19:32:59 -0500

    ensured that we reset the list of keys before performing a key exchange, just in case it's being called repeatedly or more participants have been added

--- src/DiffieHellman.scala
+++ src/DiffieHellman.scala
@@ -9,7 +9,11 @@ class DiffieHellman (val p: BigInt, val g: Int) {
   // private key is generated once and for always
   // (re-generating the private key breaks the algorithm)
   private val privateKey: BigInt = this.random(1024)
-  var peerPublicKeys: Array[BigInt] = Array[BigInt]()
+
+  // initialize the list in which we will store all
+  // our shared keys
+  var peerPublicKeys: Array[BigInt] = resetKeyList
+  def resetKeyList = Array[BigInt]()
 
   // public key is g^privateKey mod p
   def publicKey: BigInt = {
@@ -22,9 +26,10 @@ class DiffieHellman (val p: BigInt, val g: Int) {
     this.publicKey * peerPublicKeys.foldLeft(1: BigInt)((a, b) => a * b)
   }
 
-  // append the new key to the list of keys we have
-  // this operation should really only be undertaken by the
-  // DiffieHellman.doKeyExchange function.
+  // append the new key to the list of keys we have.
+  // (this operation should really only be undertaken by the
+  // DiffieHellman.doKeyExchange function to prevent duplicate
+  // key sharing)
   private def addPeerPublicKey(key: BigInt) = {
     peerPublicKeys :+= key
   }
@@ -47,6 +52,12 @@ object DiffieHellman {
   // share all public keys with all participants,
   // for any number of participants
   def doKeyExchange(participants: Array[DiffieHellman]): Unit = {
+    // first reset all the key lists, in case we're calling
+    // this function repeatedly
+    for (participant <- participants) {
+      participant.resetKeyList
+    }
+
     for {
       keySender: DiffieHellman <- participants
       keyReciever: DiffieHellman <- participants
